fcc423a79ed3fe0a180a5ecb0b89936e8b59aba9
Seth Chisamore <schisamo@getchef.com>
2015-01-23 12:51:14 -0500

    Properly wrap helper functions in a module

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -1,12 +1,19 @@
 
-def current_dir
-  @current_dir ||= Chef::Config.chef_repo_path
+module DeliveryCluster
+  module Helper
+    def current_dir
+      @current_dir ||= Chef::Config.chef_repo_path
+    end
+
+    def tmp_infra_dir
+      @tmp_infra_dir ||= File.join(current_dir, "infra/tmp")
+    end
+
+    def dot_chef_dir
+      @dot_chef_dir ||= File.join(current_dir, '.chef')
+    end
+  end
 end
 
-def tmp_infra_dir
-  @tmp_infra_dir ||= File.join(current_dir, "infra/tmp")
-end
-
-def dot_chef_dir
-  @dot_chef_dir ||= File.join(current_dir, '.chef')
-end
+Chef::Recipe.send(:include, DeliveryCluster::Helper)
+Chef::Resource.send(:include, DeliveryCluster::Helper)
