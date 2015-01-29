961fdaf9f7f58ae0f043050a1a797f75b0863936
Seth Chisamore <schisamo@getchef.com>
2015-01-29 05:55:03 -0500

    Write the correct delivery.pem in the encrypted data bag item

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -118,8 +118,8 @@ end
 
 chef_data_bag_item "keys/delivery_builder_keys" do
   raw_data(
-    delivery_pem: builder_key.to_pem.to_s
     builder_key:  builder_key.to_pem.to_s,
+    delivery_pem: "#{tmp_infra_dir}/delivery.pem"
   )
   secret_path "#{tmp_infra_dir}/encrypted_data_bag_secret"
   encryption_version 1
