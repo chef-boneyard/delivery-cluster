6dab1ea1eeff6643812f5a1f5670adb0cfb13572
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-12 18:23:07 -0400

    delivery-cli is only available from packagecloud for debian, not rhel

--- recipes/provisioning_node.rb
+++ recipes/provisioning_node.rb
@@ -37,8 +37,19 @@ directory '/home/vagrant/delivery-cluster/environments' do
   action :create
 end
 
-package 'delivery-cli' do
-  action :upgrade
+case node['platform_family']
+when 'debian'
+  package 'delivery-cli' do
+    action :upgrade
+  end
+when 'rhel'
+  execute 'installing-delivery-cli' do
+    command <<-EOF
+      curl -o delivery-cli.rpm https://s3.amazonaws.com/delivery-packages/cli/delivery-cli-20150408004719-1.x86_64.rpm
+      sudo yum install delivery-cli.rpm -y
+    EOF
+    not_if('which delivery')
+  end
 end
 
 # setup ssh keys
