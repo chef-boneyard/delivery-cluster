239211e9c00428ca653c5d20bae2b773371f8f32
Stephen Lauck <stephen.lauck@gmail.com>
2015-05-13 16:08:50 -0400

    remove redundant ssh keys code

--- recipes/provisioning_node.rb
+++ recipes/provisioning_node.rb
@@ -35,27 +35,3 @@ when 'rhel'
     not_if('which delivery')
   end
 end
-
-# setup ssh keys
-directory '/home/vagrant/.ssh' do
-  action :create
-  owner 'vagrant'
-  group 'vagrant'
-  mode '0700'
-end
-
-cookbook_file '/home/vagrant/.ssh/insecure_private_key' do
-  action :create
-  owner 'vagrant'
-  group 'vagrant'
-  mode '0600'
-  source 'insecure_private_key'
-end
-
-cookbook_file '/home/vagrant/.ssh/authorized_keys' do
-  action :create
-  owner 'vagrant'
-  group 'vagrant'
-  mode '0600'
-  source 'insecure_public_key'
-end
