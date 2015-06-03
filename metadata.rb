62febcfc1de75c87e34652ad3420a3ae60b502f0
Salim Afiune <afiune@chef.io>
2015-06-03 09:48:06 -0500

    Run delivery-ctl reconfigure after a delivery upgrade
    As a user I expect to run a `delivery-ctl reconfigure` when delivery get upgraded using delivery-cluster, so I don't have to run it manually.
    
    Fixing Issue https://github.com/opscode-cookbooks/delivery-cluster/issues/129

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Deployment cookbook for standing up Delivery Clusters'
 long_description 'Installs Chef Delivery, a solution for continuously ' \
                  'delivering applications and infrastructure safely at speed'
-version '0.2.20'
+version '0.2.21'
 
 depends 'chef-server-12'
 depends 'chef-server-ingredient'
