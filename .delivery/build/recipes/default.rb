b64a8d2d27ef61ca3ed51535e3a2636b55699dcf
Stephen Lauck <stephen.lauck@gmail.com>
2015-06-03 17:12:32 -0400

    remove string interpolation when not needed pin builder node for unit phase
    going crazy with Salim to be able to delete the cluster in aws on each change

--- .delivery/build/recipes/default.rb
+++ .delivery/build/recipes/default.rb
@@ -5,3 +5,9 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 include_recipe 'delivery-truck::default'
+
+# copy the nodes and clients dir outside of workspace
+directory "/var/opt/delivery/workspace/delivery-cluster-aws-cache" do
+  owner node['delivery_builder']['build_user']
+  group node['delivery_builder']['build_user']
+end
--- .delivery/build/recipes/provision.rb
+++ .delivery/build/recipes/provision.rb
@@ -1,4 +1,5 @@
 
+
 delivery_secrets = get_project_secrets
 
 directory File.join(node['delivery']['workspace']['cache'], '.ssh')
@@ -49,18 +50,30 @@ s3_file "#{node['delivery']['workspace']['cache']}/delivery.license" do
 end
 
 execute "chef exec bundle install" do
-  cwd "#{node['delivery']['workspace']['repo']}"
+  cwd node['delivery']['workspace']['repo']
   action :run
 end
 
 execute "chef exec bundle exec berks vendor cookbooks" do
-  cwd "#{node['delivery']['workspace']['repo']}"
+  cwd node['delivery']['workspace']['repo']
   action :run
+end
+
+execute "restore nodes and clients from outside workspace" do
+  command <<-EOF
+    mv /var/opt/delivery/workspace/delivery-cluster-aws-cache/clients clients
+    mv /var/opt/delivery/workspace/delivery-cluster-aws-cache/nodes nodes
+    mv /var/opt/delivery/workspace/delivery-cluster-aws-cache/delivery-cluster-data-aws .chef/delivery-cluster-data-aws
+    rm -rf /var/opt/delivery/workspace/delivery-cluster-aws-cache
+  EOF
+  cwd node['delivery']['workspace']['repo']
+  action :run
+  only_if do ::File.exists?('var/opt/delivery/workspace/delivery-cluster-aws-cache/nodes') end
 end
 
 # destroy everything
 execute "chef exec bundle exec chef-client -z -o delivery-cluster::destroy_all -E aws" do
-  cwd "#{node['delivery']['workspace']['repo']}"
+  cwd node['delivery']['workspace']['repo']
   environment (
     {
       'CHEF_ENV' => 'aws', 
@@ -71,13 +84,21 @@ execute "chef exec bundle exec chef-client -z -o delivery-cluster::destroy_all -
 end
 
 execute "chef exec bundle exec chef-client -z -o delivery-cluster::setup -E aws" do
-  cwd "#{node['delivery']['workspace']['repo']}"
+  cwd node['delivery']['workspace']['repo']
   environment (
     {
       'CHEF_ENV' => 'aws', 
       'AWS_CONFIG_FILE' => "#{node['delivery']['workspace']['cache']}/.aws/config"
     }
   )
+  action :run
+end
+
+execute "copy the nodes and clients dir outside of workspace" do
+  command <<-EOF
+    cp -r clients nodes .chef/delivery-cluster-data-aws /var/opt/delivery/workspace/delivery-cluster-aws-cache/
+  EOF
+  cwd node['delivery']['workspace']['repo']
   action :run
 end
 
