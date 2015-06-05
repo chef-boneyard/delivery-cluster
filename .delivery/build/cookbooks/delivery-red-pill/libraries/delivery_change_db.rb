f8ca023ae795d4430a6547229c924989df81cbcc
Jon Morrow <jon@morrowmail.com>
2015-06-05 15:47:16 -0700

    Need to demarshal 'data' from the databag.

--- .delivery/build/cookbooks/delivery-red-pill/libraries/delivery_change_db.rb
+++ .delivery/build/cookbooks/delivery-red-pill/libraries/delivery_change_db.rb
@@ -62,7 +62,7 @@ class Chef
         ::Chef_Delivery::ClientHelper.enter_client_mode_as_delivery
         node.run_state['delivery'] ||= {}
         node.run_state['delivery']['change'] ||= {}
-        node.run_state['delivery']['change']['data'] = data_bag_item('changes', change_id)
+        node.run_state['delivery']['change']['data'] ||= data_bag_item('changes', change_id)['data']
         ::Chef_Delivery::ClientHelper.leave_client_mode_as_delivery
       end
 
