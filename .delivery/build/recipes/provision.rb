814222e6682a4caf5608aef2707ac741c2499693
Stephen Lauck <stephen.lauck@gmail.com>
2015-06-05 09:37:49 -0400

    disable redpill matrix provisiong and functional

--- .delivery/build/recipes/provision.rb
+++ .delivery/build/recipes/provision.rb
@@ -4,6 +4,8 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-## By including this recipe we trigger a matrix of acceptance envs specified
-## in the node attribute node['delivery-red-pill']['acceptance']['matrix']
-include_recipe "delivery-red-pill::provision"
+# ## By including this recipe we trigger a matrix of acceptance envs specified
+# ## in the node attribute node['delivery-red-pill']['acceptance']['matrix']
+# include_recipe "delivery-red-pill::provision"
+
+include_recipe 'build::provision_clean_aws'
