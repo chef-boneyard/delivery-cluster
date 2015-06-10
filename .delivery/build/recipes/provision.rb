e68389d255e72395da3633d66d2ba31d29fa0765
Jon Morrow <jon@morrowmail.com>
2015-06-10 12:04:09 -0700

    Adding a proper smoke test
    - Added new delivery-sugar-extras library with experimental
      stuff we may want to put into delivery-sugar eventually.
    - Added a smoke recipe
    - Added logic to parse the server created and store the data
      on chef server in a databag.

--- .delivery/build/recipes/provision.rb
+++ .delivery/build/recipes/provision.rb
@@ -23,3 +23,40 @@ end
 # By including this recipe we trigger a matrix of acceptance envs specified
 # in the node attribute node['delivery-red-pill']['acceptance']['matrix']
 include_recipe "delivery-red-pill::provision"
+
+if node['delivery']['change']['pipeline'] != 'master'
+  ruby_block "Get Services" do
+    block do
+      list_services = Mixlib::ShellOut.new("rake info:list_core_services",
+                                           :cwd => node['delivery']['workspace']['repo'])
+
+      list_services.run_command
+
+      if list_services.stdout
+        node.run_state['delivery'] ||= {}
+        node.run_state['delivery']['stage'] ||= {}
+        node.run_state['delivery']['stage']['data'] ||= {}
+
+        previous_line = nil
+        list_services.stdout.each_line.chomp do |line|
+          if previous_line =~ /^delivery-server\S+:$/
+            ipaddress = line.match(/^  ipaddress:(\S+)$/])[1]
+            node.run_state['delivery']['stage']['data']['servers']['delivery_server'] = ipaddress
+          elsif previous_line =~ /^build-node\S+:/
+            ipaddress = line.match(/^  ipaddress:(\S+)$/)[1]
+            node.run_state['delivery']['stage']['data']['servers']['build_nodes'] ||= []
+            node.run_state['delivery']['stage']['data']['servers']['build_nodes'] << ipaddress
+          elsif line =~ /^chef_server_url.*$/
+            ipaddress = line.match(/^chef_server_url\s+'(\S+)'$/)
+            node.run_state['delivery']['stage']['data']['servers']['chef_server'] = ipaddress
+          end
+          previous_line = line
+        end
+      end
+    end
+  end
+
+  delivery_stage_db
+end
+
+noide.run_state[del][stage][data] = {"services" => {"chef_server_url"}
