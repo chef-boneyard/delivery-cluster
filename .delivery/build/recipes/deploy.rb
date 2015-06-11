dea6056b7fc74ee3546879e5dc68c59cfdf856b2
Jon Morrow <jon@morrowmail.com>
2015-06-11 13:05:20 -0700

    Only run deploy if we are not and master and in acceptance.

--- .delivery/build/recipes/deploy.rb
+++ .delivery/build/recipes/deploy.rb
@@ -3,27 +3,30 @@
 # Recipe:: deploy
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
-cluster_name = "#{node['delivery']['change']['stage']}_#{node['delivery']['change']['pipeline']}"
-cache = node['delivery']['workspace']['cache']
-path = node['delivery']['workspace']['repo']
+if node['delivery']['change']['pipeline'] != 'master'
+   && node['delivery']['change']['stage'] == 'acceptance'
+  cluster_name = "#{node['delivery']['change']['stage']}_#{node['delivery']['change']['pipeline']}"
+  cache = node['delivery']['workspace']['cache']
+  path = node['delivery']['workspace']['repo']
 
-template "Update Environment Template" do
-  path File.join(path, "environments/#{cluster_name}.json")
-  source 'environment.json.erb'
-  variables(
-    :delivery_license => "#{cache}/delivery.license",
-    :delivery_version => "latest",
-    :cluster_name => cluster_name
-  )
-end
+  template "Update Environment Template" do
+    path File.join(path, "environments/#{cluster_name}.json")
+    source 'environment.json.erb'
+    variables(
+      :delivery_license => "#{cache}/delivery.license",
+      :delivery_version => "latest",
+      :cluster_name => cluster_name
+    )
+  end
 
-execute "Upgrade Delivery Cluster To Latest" do
-  cwd path
-  command <<-EOF
-    chef exec bundle exec chef-client -z -o delivery-cluster::setup -E #{cluster_name} -l auto > #{cache}/delivery-cluster-setup.log
-    cp -r clients nodes .chef/delivery-cluster-data-* .chef/trusted_certs /var/opt/delivery/workspace/delivery-cluster-aws-cache/
-  EOF
-  environment ({
-    'AWS_CONFIG_FILE' => "#{cache}/.aws/config"
-  })
+  execute "Upgrade Delivery Cluster To Latest" do
+    cwd path
+    command <<-EOF
+      chef exec bundle exec chef-client -z -o delivery-cluster::setup -E #{cluster_name} -l auto > #{cache}/delivery-cluster-setup.log
+      cp -r clients nodes .chef/delivery-cluster-data-* .chef/trusted_certs /var/opt/delivery/workspace/delivery-cluster-aws-cache/
+    EOF
+    environment ({
+      'AWS_CONFIG_FILE' => "#{cache}/.aws/config"
+    })
+  end
 end
