edebb97e74bd769725ca83ad8f917f37425cbc4c
Jon Morrow <jon@morrowmail.com>
2015-06-11 09:27:30 -0700

    Mved github publish to delivered:functional, fixed smoke.

--- .delivery/build/recipes/functional.rb
+++ .delivery/build/recipes/functional.rb
@@ -7,3 +7,19 @@
 # ## By including this recipe we trigger a matrix of acceptance envs specified
 # ## in the node attribute node['delivery-red-pill']['acceptance']['matrix']
 include_recipe "delivery-red-pill::functional"
+
+if node['delivery']['change']['pipeline'] == 'master' &&
+   node['delivery']['change']['stage'] == 'delivered'
+  # This was lifted from delivery-truck publish
+  secrets = get_project_secrets
+  github_repo = node['delivery']['config']['delivery-truck']['publish']['github']
+
+  delivery_github github_repo do
+    deploy_key secrets['github']
+    branch node['delivery']['change']['pipeline']
+    remote_url "git@github.com:#{github_repo}.git"
+    repo_path node['delivery']['workspace']['repo']
+    cache_path node['delivery']['workspace']['cache']
+    action :push
+  end
+end
