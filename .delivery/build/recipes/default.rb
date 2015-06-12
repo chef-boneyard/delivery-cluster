204303517d93e3329045d2d339d930c043f7fb28
Jon Morrow <jon@morrowmail.com>
2015-06-12 13:12:33 -0700

    use class ivars in provider.

--- .delivery/build/recipes/default.rb
+++ .delivery/build/recipes/default.rb
@@ -4,15 +4,16 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-include_recipe 'delivery-sugar-extras::default'
-include_recipe 'delivery-red-pill::default'
-include_recipe 'delivery-truck::default'
-
+# Gems for our tests
 %w{watir-webdriver phantomjs}.each do |g|
   chef_gem g do
     compile_time true
   end
 end
+
+include_recipe 'delivery-sugar-extras::default'
+include_recipe 'delivery-red-pill::default'
+include_recipe 'delivery-truck::default'
 
 # Temporal cache directory to store delivery-cluster-data
 directory "/var/opt/delivery/workspace/delivery-cluster-aws-cache" do
