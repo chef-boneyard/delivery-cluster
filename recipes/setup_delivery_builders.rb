a0156cee6d44f5e1f2aef354beaec9e05867ec1b
Salim Afiune <afiune@chef.io>
2015-10-05 11:07:57 -0500

    Ensure all files get uploaded to the build-nodes
    The `machine` resource in `chef-provisioning` does not allow to have
    `files` and `file` statements together. That means that one of them will
    take precedence.
    
    This commit fixes the problem by abstracting the files to upload into a
    library in `libraries/helpers_builders.rb` that will lazy load to ensure
    that we upload all the files (even the ones that get render on the
    chef-zero run) to the build-nodes
    
    Closes https://github.com/chef-cookbooks/delivery-cluster/issues/164

--- recipes/setup_delivery_builders.rb
+++ recipes/setup_delivery_builders.rb
@@ -45,15 +45,7 @@ machine_batch "#{node['delivery-cluster']['builders']['count']}-build-nodes" do
       provisioning.specific_machine_options('builders', i).each do |option|
         add_machine_options option
       end
-      Dir.glob("#{Chef::Config[:trusted_certs_dir]}/*").each do |cert_path|
-        file ::File.join('/etc/chef/trusted_certs', ::File.basename(cert_path)), cert_path
-      end
-      files lazy {
-        {
-          "/etc/chef/trusted_certs/#{delivery_server_fqdn}.crt" => "#{Chef::Config[:trusted_certs_dir]}/#{delivery_server_fqdn}.crt",
-          '/etc/chef/encrypted_data_bag_secret' => "#{cluster_data_dir}/encrypted_data_bag_secret"
-        }
-      }
+      files lazy { builders_files }
       attributes lazy { builders_attributes }
       converge true
       action :converge
