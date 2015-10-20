00c02714bdb568162902a65d803bff650c64ecee
Salim Afiune <afiune@chef.io>
2015-10-19 22:04:47 -0400

    Chefspec for Supermarket & Analytics Recipes

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -10,4 +10,16 @@ if defined?(ChefSpec)
   def run_machine_execute(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:machine_execute, :run, resource_name)
   end
+
+  def install_chef_ingredient(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:chef_ingredient, :install, resource_name)
+  end
+
+  def reconfigure_chef_ingredient(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:chef_ingredient, :reconfigure, resource_name)
+  end
+
+  def add_ingredient_config(resource_name)
+    ChefSpec::Matchers::ResourceMatcher.new(:ingredient_config, :add, resource_name)
+  end
 end
