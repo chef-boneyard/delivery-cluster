40b94144d93aa91d74e01b3e103eceac158f26ad
David McCown <dmccown@chef.io>
2016-04-14 17:28:11 -0500

    Attempting to disable the matrix build
    Temporarily disabling the matrix build to unblock pipeline.

--- .delivery/build/attributes/default.rb
+++ .delivery/build/attributes/default.rb
@@ -3,6 +3,7 @@ include_attribute 'delivery-matrix'
 # Turning these off for now becuase of some build failures related to
 # the artifactory switchover. Will likely need a fix to delivery-sugar-extras to fix
 
+default['delivery-matrix']['acceptance']['matrix'] = []
 
 #default['delivery-matrix']['acceptance']['matrix'] = [
 #  "clean_aws",
