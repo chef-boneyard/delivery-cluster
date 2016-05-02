521b3cfb5db5476ae900f66f09f932b18c2d62c0
Salim Afiune <afiune@chef.io>
2016-05-02 18:37:58 +0200

    Accept Chef License for the pipeline

--- .delivery/build/metadata.rb
+++ .delivery/build/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'delivery-team@chef.io'
 license 'Apache 2.0'
 description 'Delivery Build Cookbook for Delivery Clusters'
 long_description 'Delivery Build Cookbook for Delivery Clusters'
-version '1.3.4'
+version '1.3.5'
 
 depends 'delivery-truck'
 depends 's3_file'
