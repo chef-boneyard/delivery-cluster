fabac3168ad2e3a064746dcb5763bc2050c2b9dc
Salim Afiune <afiune@chef.io>
2016-05-19 15:09:00 -0400

    Bump version to 0.6.12

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'Apache 2.0'
 description 'Deployment cookbook for standing up Delivery Clusters'
 long_description 'Installs Chef Delivery, a solution for continuously ' \
                  'delivering applications and infrastructure safely at speed'
-version '0.6.11'
+version '0.6.12'
 
 depends 'chef-server-12'
 depends 'chef-ingredient', '>= 0.18.0'
