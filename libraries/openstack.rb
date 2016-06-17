bd57059b085ce8c7a9bd153f1b2972a64bcd5561
Chris McClimans <c@vulk.coop>
2016-06-17 01:12:28 +0000

    first stab at openstack support

---
+++ libraries/openstack.rb
@@ -0,0 +1,115 @@
+#
+# Cookbook Name:: delivery-cluster
+# Library:: openstack 
+#
+# Author:: Chris McClimans (<c@vulk.coop>)
+#
+# Copyright:: Copyright (c) 2016 Vulk Coop
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require_relative '_base'
+
+module DeliveryCluster
+  module Provisioning
+    #
+    # AWS class for AWS Provisioning Driver
+    #
+    # Specify all the methods a Provisioning Driver should implement
+    # @author Salim Afiune <afiune@chef.io>
+    class Openstack < DeliveryCluster::Provisioning::Base
+      attr_accessor :node
+      attr_accessor :ssh_username
+      alias username ssh_username
+
+      # Create a new Provisioning Driver Abstraction
+      #
+      # @param node [Chef::Node]
+      def initialize(node)
+        require 'chef/provisioning/fog_driver/driver'
+        require 'chef/provisioning/fog_driver/providers/openstack'
+
+        DeliveryCluster::Helpers.check_attribute?(node['delivery-cluster'][driver], "node['delivery-cluster']['#{driver}']")
+        @node            = node
+        @driver_hash     = @node['delivery-cluster'][driver]
+
+        @driver_hash.each do |attr, value|
+          singleton_class.class_eval { attr_accessor attr }
+          instance_variable_set("@#{attr}", value)
+        end
+      end
+
+      # Return the machine options to use.
+      #
+      # @return [Hash] the machine_options for the specific driver
+      def machine_options
+        opts = {
+          convergence_options: {
+            bootstrap_proxy: @bootstrap_proxy,
+            chef_config: @chef_config,
+            chef_version: @chef_version,
+            install_sh_path: @install_sh_path
+          },
+          bootstrap_options: {
+            flavor_ref:         @flavor_ref,
+            image_ref:               @image_ref,
+            key_name:           @key_name,
+            security_groups: @security_groups
+          },
+          ssh_username:           @ssh_username,
+          use_private_ip_for_ssh: @use_private_ip_for_ssh
+        }
+
+        # Add any optional machine options
+        require 'chef/mixin/deep_merge'
+        opts = Chef::Mixin::DeepMerge.hash_only_merge(opts, bootstrap_options: { subnet_id: @subnet_id }) if @subnet_id
+        #require 'pry-byebug' ; binding.pry
+
+        opts
+      end
+
+      # Create a array of machine_options specifics to a component
+      #
+      # @param component [String] component name
+      # @param count [Integer] component number
+      # @return [Array] specific machine_options for the specific component
+      def specific_machine_options(component, _count = nil)
+        return [] unless @node['delivery-cluster'][component]
+        options = []
+        options << { bootstrap_options: { instance_type: @node['delivery-cluster'][component]['flavor_ref'] } } if @node['delivery-cluster'][component]['flavor_ref']
+        options << { bootstrap_options: { security_group_ids: @node['delivery-cluster'][component]['security_group_ids'] } } if @node['delivery-cluster'][component]['security_group_ids']
+        options << { image_id: @node['delivery-cluster'][component]['image_id'] } if @node['delivery-cluster'][component]['image_id']
+        options << { aws_tags: @node['delivery-cluster'][component]['aws_tags'] } if @node['delivery-cluster'][component]['aws_tags']
+        # Specify more specific machine_options to add
+        options
+      end
+
+      # Return the Provisioning Driver Name.
+      #
+      # @return [String] the provisioning driver name
+      def driver
+        'fog:OpenStack'
+      end
+
+      # Return the ipaddress from the machine.
+      #
+      # @param node [Chef::Node]
+      # @return [String] an ipaddress
+      def ipaddress(node)
+        @use_private_ip_for_ssh ? node['ec2']['local_ipv4'] : node['ec2']['public_ipv4']
+      end
+    end
+  end
+end
--- libraries/provisioning.rb
+++ libraries/provisioning.rb
@@ -19,6 +19,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+require 'chef/provisioning'
 module DeliveryCluster
   #
   # Module to create instances of Provisioning Drivers
